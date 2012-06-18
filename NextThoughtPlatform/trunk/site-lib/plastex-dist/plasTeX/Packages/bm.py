1a10eb8fc8757b8a48ce521e9e032eff9001b294
sean.jones <sean.jones@84ec493d-749a-4728-a00c-35e79d17dfe8>
2012-06-18 21:03:58 +0000

    Moved per direction from Jason.

--- NextThoughtPlatform/trunk/site-lib/plastex-dist/plasTeX/Packages/bm.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python                                                                                                   
-
-from plasTeX import Command, Environment
-
-class bm(Command):
-    args = 'self'
-    
