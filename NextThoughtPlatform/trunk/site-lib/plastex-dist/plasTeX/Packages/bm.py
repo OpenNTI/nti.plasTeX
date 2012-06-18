d49d1fd059382f8d5a571717898667944d380f27
sean.jones <sean.jones@84ec493d-749a-4728-a00c-35e79d17dfe8>
2012-06-18 20:20:10 +0000

    Added support for the bm LaTex package. Updated unit tests are required.

---
+++ NextThoughtPlatform/trunk/site-lib/plastex-dist/plasTeX/Packages/bm.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python                                                                                                   
+
+from plasTeX import Command, Environment
+
+class bm(Command):
+    args = 'self'
+    
