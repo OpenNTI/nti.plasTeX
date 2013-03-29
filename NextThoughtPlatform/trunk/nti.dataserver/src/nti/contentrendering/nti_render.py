4d34c8c5bbb6b29386a60230122663807d8c7761
carlos.sanchez <carlos.sanchez@84ec493d-749a-4728-a00c-35e79d17dfe8>
2013-03-29 22:04:52 +0000

    add video transcript indexing to indexing pipeline.

--- NextThoughtPlatform/trunk/nti.dataserver/src/nti/contentrendering/nti_render.py
+++ NextThoughtPlatform/trunk/nti.dataserver/src/nti/contentrendering/nti_render.py
@@ -52,8 +52,8 @@ from nti.contentrendering.resources.ResourceRenderer import createResourceRender
 
 
 def _configure_logging():
-	logging.basicConfig( level=logging.INFO )
-	logging.root.handlers[0].setFormatter( zope.exceptions.log.Formatter( '[%(name)s] %(levelname)s: %(message)s' ) )
+	logging.basicConfig(level=logging.INFO)
+	logging.root.handlers[0].setFormatter(zope.exceptions.log.Formatter('[%(name)s] %(levelname)s: %(message)s'))
 
 def _catching(f):
 	@functools.wraps(f)
@@ -61,11 +61,11 @@ def _catching(f):
 		try:
 			f()
 		except subprocess.CalledProcessError as spe:
-			logger.exception( "Failed to run subprocess" )
-			sys.exit( spe.returncode )
+			logger.exception("Failed to run subprocess")
+			sys.exit(spe.returncode)
 		except:
-			logger.exception( "Failed to run main" )
-			sys.exit( 1 )
+			logger.exception("Failed to run main")
+			sys.exit(1)
 	return y
 
 
@@ -77,18 +77,18 @@ def main():
 
 
 	sourceFile = argv.pop(0)
-	source_dir = os.path.dirname( os.path.abspath( os.path.expanduser( sourceFile ) ) )
+	source_dir = os.path.dirname(os.path.abspath(os.path.expanduser(sourceFile)))
 
 	outFormat = 'xhtml'
 	if argv:
 		outFormat = argv.pop(0)
 
-	zope_pre_conf_name = os.path.join( source_dir, 'pre_configure.zcml' )
+	zope_pre_conf_name = os.path.join(source_dir, 'pre_configure.zcml')
 	xml_conf_context = None
-	if os.path.exists( zope_pre_conf_name ):
-		xml_conf_context = xmlconfig.file( os.path.abspath( zope_pre_conf_name ), package=nti.contentrendering )
+	if os.path.exists(zope_pre_conf_name):
+		xml_conf_context = xmlconfig.file(os.path.abspath(zope_pre_conf_name), package=nti.contentrendering)
 
-	xml_conf_context = xmlconfig.file( 'configure.zcml', package=nti.contentrendering, context=xml_conf_context )
+	xml_conf_context = xmlconfig.file('configure.zcml', package=nti.contentrendering, context=xml_conf_context)
 
 	# Set up imports for style files. The preferred, if verbose, way is to
 	# use a fully qualified python name. But for legacy and convenience
@@ -96,8 +96,8 @@ def main():
 	# by adding that directory directly to the path
 	# Note that the cwd is on the path by default and files there
 	# (per-job) should take precedence
-	packages_path = os.path.join( os.path.dirname( __file__ ) , 'plastexpackages' )
-	sys.path.append( packages_path )
+	packages_path = os.path.join(os.path.dirname(__file__) , 'plastexpackages')
+	sys.path.append(packages_path)
 
 
 	# Create document instance that output will be put into
@@ -107,12 +107,12 @@ def main():
 	# Certain things like to assume that the root document is called index.html. Make it so.
 	# This is actually plasTeX.Base.LaTeX.Document.document, but games are played
 	# with imports. damn it.
-	plasTeX.Base.document.filenameoverride = property(lambda s: 'index') #.html added automatically
+	plasTeX.Base.document.filenameoverride = property(lambda s: 'index')  # .html added automatically
 
 
-	#setup default config options we want
+	# setup default config options we want
 	document.config['files']['split-level'] = 1
-	document.config['document']['toc-depth'] = sys.maxint # Arbitrary number greater than the actual depth possible
+	document.config['document']['toc-depth'] = sys.maxint  # Arbitrary number greater than the actual depth possible
 	document.config['document']['toc-non-files'] = True
 	# By outputting in ASCII, we are still valid UTF-8, but we use
 	# XML entities for high characters. This is more likely to survive
@@ -121,17 +121,17 @@ def main():
 	document.config['general']['theme'] = 'NTIDefault'
 	document.config['general']['theme-base'] = 'NTIDefault'
 	# Read a config if present
-	document.config.add_section( 'NTI' )
-	document.config.set( 'NTI', 'provider', os.environ.get( 'NTI_PROVIDER', 'AOPS' ) )
-	document.config.set( 'NTI', 'extra-scripts', '' )
-	document.config.set( 'NTI', 'extra-styles', '' )
-	conf_name = os.path.join( source_dir, "nti_render_conf.ini" )
-	document.config.read( (conf_name,) )
+	document.config.add_section('NTI')
+	document.config.set('NTI', 'provider', os.environ.get('NTI_PROVIDER', 'AOPS'))
+	document.config.set('NTI', 'extra-scripts', '')
+	document.config.set('NTI', 'extra-styles', '')
+	conf_name = os.path.join(source_dir, "nti_render_conf.ini")
+	document.config.read((conf_name,))
 
 	# Configure components and utilities
-	zope_conf_name = os.path.join( source_dir, 'configure.zcml' )
-	if os.path.exists( zope_conf_name ):
-		xml_conf_context = xmlconfig.file( os.path.abspath( zope_conf_name ), package=nti.contentrendering, context=xml_conf_context )
+	zope_conf_name = os.path.join(source_dir, 'configure.zcml')
+	if os.path.exists(zope_conf_name):
+		xml_conf_context = xmlconfig.file(os.path.abspath(zope_conf_name), package=nti.contentrendering, context=xml_conf_context)
 
 	# Instantiate the TeX processor
 	tex = TeX(document, file=sourceFile)
@@ -161,30 +161,30 @@ def main():
 	# pauxname = '%s.paux' % jobname
 
 	# for dirname in [cwd] + config['general']['paux-dirs']:
-	#	for fname in glob.glob(os.path.join(dirname, '*.paux')):
-	#		if os.path.basename(fname) == pauxname:
-	#			continue
-	#		document.context.restore(fname, rname)
+	# 	for fname in glob.glob(os.path.join(dirname, '*.paux')):
+	# 		if os.path.basename(fname) == pauxname:
+	# 			continue
+	# 		document.context.restore(fname, rname)
 
 
 	# Set up TEXINPUTS to include the current directory for the renderer,
 	# plus our packages directory
 	texinputs = (os.getcwd(), source_dir, packages_path, os.environ.get('TEXINPUTS', ''))
-	os.environ['TEXINPUTS'] = os.path.pathsep.join( texinputs )
+	os.environ['TEXINPUTS'] = os.path.pathsep.join(texinputs)
 
 	# Likewise for the renderers, with the addition of the legacy 'zpts' directory.
 	# Parts of the code (notably tex2html._find_theme_mathjaxconfig) depend on
 	# the local Template being first. Note that earlier values will take precedence
 	# over later values.
-	xhtmltemplates = (os.path.join( os.getcwd(), 'Templates' ),
+	xhtmltemplates = (os.path.join(os.getcwd(), 'Templates'),
 					  packages_path,
-					  resource_filename( __name__, 'zpts' ),
+					  resource_filename(__name__, 'zpts'),
 					  os.environ.get('XHTMLTEMPLATES', ''))
-	os.environ['XHTMLTEMPLATES'] = os.path.pathsep.join( xhtmltemplates )
+	os.environ['XHTMLTEMPLATES'] = os.path.pathsep.join(xhtmltemplates)
 	setupChameleonCache(config=True)
 
 	# Parse the document
-	logger.info( "Parsing %s", sourceFile )
+	logger.info("Parsing %s", sourceFile)
 	tex.parse()
 
 	# Change to specified directory to output to
@@ -197,25 +197,25 @@ def main():
 		os.chdir(outdir)
 
 	# Perform prerender transforms
-	transforms.performTransforms( document, context=components )
+	transforms.performTransforms(document, context=components)
 
 	if outFormat == 'images' or outFormat == 'xhtml':
-		logger.info( "Generating images" )
+		logger.info("Generating images")
 		db = generateImages(document)
 
 	if outFormat == 'xhtml':
-		render( document, 'XHTML', db )
+		render(document, 'XHTML', db)
 		postRender(document, jobname=jobname, context=components)
 
 	if outFormat == 'xml':
-		toXml( document, jobname )
+		toXml(document, jobname)
 
-	write_dc_metadata( document, jobname )
+	write_dc_metadata(document, jobname)
 
 from nti.utils import setupChameleonCache
 
 def postRender(document, contentLocation='.', jobname='prealgebra', context=None):
-	logger.info( 'Performing post render steps' )
+	logger.info('Performing post render steps')
 	# FIXME: This was not particularly well thought out. We're using components,
 	# but named utilities, not generalized adapters or subscribers.
 	# That makes this not as extensible as it should be.
@@ -226,64 +226,66 @@ def postRender(document, contentLocation='.', jobname='prealgebra', context=None
 
 	# This step adds NTIIDs to the TOC in addition to modifying
 	# on-disk content.
-	logger.info( 'Adding icons to toc and pages' )
+	logger.info('Adding icons to toc and pages')
 	tociconsetter.transform(book, context=context)
 
-	logger.info( 'Fetching page info' )
+	logger.info('Fetching page info')
 	book = RenderedBook(document, contentLocation)
 
-	logger.info( 'Storing content height in pages' )
+	logger.info('Storing content height in pages')
 	contentsizesetter.transform(book, context=context)
 
-	logger.info( 'Adding related links to toc' )
+	logger.info('Adding related links to toc')
 	relatedlinksetter.performTransforms(book, context=context)
 
-	logger.info( 'Generating thumbnails for pages' )
-	contentthumbnails.transform(book,context=context)
+	logger.info('Generating thumbnails for pages')
+	contentthumbnails.transform(book, context=context)
 
 	# PhantomJS doesn't cope well with the iframes
 	# for embedded videos: you get a black box, and we put them at the top
 	# of the pages, so many thumbnails end up looking the same, and looking
 	# bad. So do this after taking thumbnails.
-	logger.info( 'Adding videos' )
-	sectionvideoadder.performTransforms(book,context=context)
+	logger.info('Adding videos')
+	sectionvideoadder.performTransforms(book, context=context)
 
-	logger.info( 'Running checks on content' )
-	contentchecks.performChecks(book,context=context)
+	logger.info('Running checks on content')
+	contentchecks.performChecks(book, context=context)
 
 	contentPath = os.path.realpath(contentLocation)
-	if not os.path.exists( os.path.join( contentPath, 'indexdir' ) ):
+	if not os.path.exists(os.path.join(contentPath, 'indexdir')):
 		# We'd like to be able to run this with pypy (it's /much/ faster)
 		# but some of the Zope libs we import during contentsearch (notably Persistent)
 		# are not quite compatible. A previous version of this code made the correct
 		# changes PYTHONPATH changes for this to work (before contentsearch grew those deps);
 		# now it just generates exceptions, so we don't try right now
-		logger.info( "Indexing content in-process." )
+		logger.info("Indexing content in-process.")
 		# Defer the import to the last possible moment, as this pulls in a boatload of deps,
 		# including gevent patches we don't want
-		from nti.contentrendering import indexer
-		indexer.transform( book )#index_content(tocFile=toc_file, contentPath=contentPath, indexname=jobname)
+		from nti.contentrendering import book_content_indexer as book_indexer
+		from nti.contentrendering import video_transcript_indexer as video_trax_indexer
+		for indexer in (book_indexer, video_trax_indexer):
+			indexer.transform(book)
 
 	# TODO: Aren't the things in the archive mirror file the same things
 	# we want to list in the manifest? If so, we should be able to combine
 	# these steps (if nothing else, just list the contents of the archive to get the
 	# manifest)
-	logger.info( "Creating html cache-manifest" )
+	logger.info("Creating html cache-manifest")
 	html5cachefile.main(contentPath, contentPath)
 
-	logger.info( 'Changing intra-content links' )
-	ntiidlinksetter.transform( book )
+	logger.info('Changing intra-content links')
+	ntiidlinksetter.transform(book)
 
-	logger.info( "Creating a mirror file" )
-	mirror.main( contentPath, contentPath, zip_root_dir=jobname )
+	logger.info("Creating a mirror file")
+	mirror.main(contentPath, contentPath, zip_root_dir=jobname)
 
-	logger.info( "Extracting assessments" )
-	extractor = component.queryUtility( interfaces.IRenderedBookTransformer, name='AssessmentExtractor' )
+	logger.info("Extracting assessments")
+	extractor = component.queryUtility(interfaces.IRenderedBookTransformer, name='AssessmentExtractor')
 	if extractor:
-		extractor.transform( book )
+		extractor.transform(book)
 
-	logger.info( "Creating JSONP content" )
-	jsonpbuilder.transform( book )
+	logger.info("Creating JSONP content")
+	jsonpbuilder.transform(book)
 
 
 def render(document, rname, db):
@@ -292,12 +294,12 @@ def render(document, rname, db):
 	renderer.render(document)
 	return renderer
 
-def toXml( document, jobname ):
+def toXml(document, jobname):
 	outfile = '%s.xml' % jobname
-	with open(outfile,'w') as f:
+	with open(outfile, 'w') as f:
 		f.write(document.toXML().encode('utf-8'))
 
-def write_dc_metadata( document, jobname ):
+def write_dc_metadata(document, jobname):
 	"""
 	Write an XML file containing the DublinCore metadata we can extract for this document.
 	"""
@@ -313,7 +315,7 @@ def write_dc_metadata( document, jobname ):
 		# DC Title is an array, latex title is scalar
 		# Sometimes title may be a string or it may be a TeXElement, depending
 		# on what packages have dorked things up
-		mapping['Title'] = (getattr( metadata['title'], 'textContent', metadata['title']),)
+		mapping['Title'] = (getattr(metadata['title'], 'textContent', metadata['title']),)
 	# The 'date' command in latex is free form, which is not
 	# what we want for DC...what do we want?
 
@@ -324,16 +326,16 @@ def write_dc_metadata( document, jobname ):
 	if not mapping:
 		return
 
-	xml_string = unicode(zope.dublincore.xmlmetadata.dumpString( mapping ))
-	with open( 'dc_metadata.xml', 'w' ) as f:
-		f.write( xml_string.encode( 'utf-8' ) )
+	xml_string = unicode(zope.dublincore.xmlmetadata.dumpString(mapping))
+	with open('dc_metadata.xml', 'w') as f:
+		f.write(xml_string.encode('utf-8'))
 
 
 def generateImages(document):
 	### Generates required images ###
 	# Replace this with configuration/use of ZCA?
-	local_overrides = os.path.join( os.getcwd(), '../nti.resourceoverrides' )
-	if os.path.exists( os.path.join(local_overrides, ResourceTypeOverrides.OVERRIDE_INDEX_NAME) ):
+	local_overrides = os.path.join(os.getcwd(), '../nti.resourceoverrides')
+	if os.path.exists(os.path.join(local_overrides, ResourceTypeOverrides.OVERRIDE_INDEX_NAME)):
 		overrides = local_overrides
 	else:
 		overrides = resource_filename(__name__, 'resourceoverrides')
