dcc9c91bd9a07cf53b5cc0265ef69c183a54b116
Jason Madden <jamadden@gmail.com>
2013-12-11 09:03:03 -0600

    More on catching the intermittant test failure I can't reproduce

--- src/plasTeX/tests/test_Crossref.py
+++ src/plasTeX/tests/test_Crossref.py
@@ -2,13 +2,14 @@
 
 from __future__ import absolute_import, division, unicode_literals
 
-import unittest
 from unittest import TestCase
 from plasTeX.TeX import TeX
 
 from hamcrest import assert_that
 from hamcrest import has_property
 from hamcrest import is_not
+from hamcrest import has_length
+
 
 class TestLabels(TestCase):
 
@@ -29,15 +30,14 @@ class TestLabels(TestCase):
 		s = TeX()
 		s.input(r'\section{hi} text \section*{bye\label{two}}')
 		output = s.parse()
-		one = output[0]
-		two = output[-1]
+		assert_that( output, has_length(2) )
+		section = output[0]
+		section_star = output[1]
 
-		__traceback_info__ = one.__dict__
-		assert_that( one, has_property( 'id', 'two' ) )
-		__traceback_info__ = two.__dict__
-		assert_that( two, has_property( 'id', is_not( 'two' ) ) )
+		__traceback_info__ = (section, section.__dict__, section_star, section_star.__dict__)
 
+		assert_that( section, has_property( 'source', '\\section{hi} text \n\n'))
+		assert_that( section, has_property( 'id', 'two' ) )
 
-
-if __name__ == '__main__':
-	unittest.main()
+		assert_that( section_star, has_property( 'source', '\\section*{bye\\label{two}}'))
+		assert_that( section_star, has_property( 'id', is_not( 'two' ) ) )
