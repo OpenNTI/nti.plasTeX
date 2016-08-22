a5fe3bcd5032533adce5747e1ab7625986b635c4
Jason Madden <jamadden@gmail.com>
2016-08-22 14:32:23 -0500

    Avoid writing into our own test directories. Fixes #3

--- src/plasTeX/tests/test_FunctionalTests.py
+++ src/plasTeX/tests/test_FunctionalTests.py
@@ -41,36 +41,37 @@ class Process(object):
 class _ComparisonBenched(object):
     """ Compile LaTeX file and compare to benchmark file """
 
-    def __call__(self, filename):
-        __traceback_info__ = filename
-        if not filename:
+    class DontCleanupError(OSError):
+        pass
+
+    def __call__(self, latex_file):
+        __traceback_info__ = latex_file
+        if not latex_file:
             return
-
-        src = filename
-
 
         # Create temp dir and files
         outdir = tempfile.mkdtemp()
-        texfile = os.path.join(outdir, os.path.basename(src))
 
         try:
-            self._compare( outdir, src, texfile )
+            self._compare(outdir, latex_file)
+        except self.DontCleanupError:
+            clean_up = False
         finally:
             # Clean up
-            shutil.rmtree(outdir, ignore_errors=True)
+            if clean_up:
+                shutil.rmtree(outdir, ignore_errors=True)
 
-    def _compare( self, outdir, src, texfile ):
-        shutil.copyfile(src, texfile)
-        root = os.path.dirname(os.path.dirname(src))
+    def __preprocess_file(self, outdir, original_source_file):
+        root = os.path.dirname(os.path.dirname(original_source_file))
 
         # Run preprocessing commands
-        with open(src, 'r') as f:
+        with open(original_source_file, 'r') as f:
             for line in f:
                 if line.startswith('%*'):
                     command = line[2:].strip()
                     p = Process(cwd=outdir, *command.split())
                     if p.returncode:
-                        raise OSError( 'Preprocessing command exited abnormally with return code %s: %s' % (command, p.log) )
+                        raise OSError('Preprocessing command exited abnormally with return code %s: %s' % (command, p.log))
                 elif line.startswith('%#'):
                     filename = line[2:].strip()
                     shutil.copyfile(os.path.join(root,'extras',filename),
@@ -82,57 +83,61 @@ class _ComparisonBenched(object):
                 else:
                     break
 
-        # Run plastex
-        outfile = os.path.join(outdir, os.path.splitext(os.path.basename(src))[0] + '.html')
+    def __run_plastex(self, outdir, outfile, original_source_file):
         plastex = which('plastex') or 'plastex'
         python = sys.executable
         p = Process(python, plastex,'--split-level=0','--no-theme-extras',
                     '--dir=%s' % outdir,'--theme=minimal',
-                    '--filename=%s' % os.path.basename(outfile), os.path.basename(src),
+                    '--filename=%s' % os.path.basename(outfile), os.path.basename(original_source_file),
                     cwd=outdir)
         if p.returncode:
-            shutil.rmtree(outdir, ignore_errors=True)
-            raise OSError( 'plastex failed with code %s: %s' % (p.returncode, p.log) )
+            raise OSError( 'plastex failed with code %s: %s' % (p.returncode, p.log))
+
+        return p.log
+
+    def __no_benchmark_file(self, outdir, outfile, benchfile):
+        raise self.DontCleanupError( 'No benchmark file: %s; new file in %s' % (benchfile, outfile) )
+
+    def __differences_found(self, outdir, outfile, benchfile, diff):
+        raise self.DontCleanupError('Diff between benchmark file (%s) and new file (%s):\n%s'
+                                    % (benchfile, outfile, diff))
+
+    def _compare(self, outdir, original_source_file):
+        orig_root = os.path.dirname(os.path.dirname(original_source_file))
+
+        outfile = os.path.join(outdir,
+                               os.path.splitext(os.path.basename(original_source_file))[0] + '.html')
+        benchfile = os.path.join(orig_root, 'benchmarks', os.path.basename(outfile))
+
+        temp_latex_file = os.path.join(outdir, os.path.basename(original_source_file))
+
+        shutil.copyfile(original_source_file, temp_latex_file)
+
+        self.__preprocess_file(outdir, original_source_file)
+
+        # Run plastex
+        log = self.__run_plastex(outdir, outfile, original_source_file)
 
         # Read output file
-        __traceback_info__ = p.log, outdir, outfile
-
-        output = open(outfile)
+        __traceback_info__ = log, outdir, outfile
 
         # Get name of output file / benchmark file
-        benchfile = os.path.join(root,'benchmarks',os.path.basename(outfile))
-        if os.path.isfile(benchfile):
-            with open(benchfile, 'r') as f:
-                bench = f.readlines()
-            outputlines = output.readlines()
-            output.close()
-            output = outputlines
-        else:
-            try:
-                os.makedirs(os.path.join(root,'new'))
-            except:
-                pass
-            newfile = os.path.join(root,'new',os.path.basename(outfile))
-            with open(newfile,'w') as f:
-                f.write(output.read())
-                output.close()
 
-            shutil.rmtree(outdir, ignore_errors=True)
-            raise OSError( 'No benchmark file: %s' % benchfile )
+        if not os.path.isfile(benchfile):
+            # Don't cleanup, let the user compare/copy the benchfile into place
+            return self.__no_benchmark_file(outdir, outfile, benchfile)
+
+
+        with open(benchfile, 'r') as f:
+            benchlines = f.readlines()
+        with open(outfile, 'r') as f:
+            outputlines = f.readlines()
 
         # Compare files
-        diff = ''.join(list(difflib.unified_diff(bench, output))).strip()
+        diff = ''.join(list(difflib.unified_diff(benchlines, outputlines))).strip()
         if diff:
-            shutil.rmtree(outdir, ignore_errors=True)
-            try:
-                os.makedirs(os.path.join(root,'new'))
-            except:
-                pass
-            newfile = os.path.join(root,'new',os.path.basename(outfile))
-            with open(newfile,'w') as f:
-                f.writelines(output)
-            assert not(diff), 'Differences were found: %s' % diff
-
+            # Don't cleanup, let the user decide to copy the new file into place
+            self._differences_found(outdir, outfile, benchfile, diff)
 
 def testSuite():
     """
