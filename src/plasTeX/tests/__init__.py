4b7034fbe4a39c96ce85121681b3ff5c4ac236dc
Jason Madden <jamadden@gmail.com>
2016-08-25 09:38:28 -0500

    Refactor tests to invoke plastex from one place
    However, this exposes a bug in test_FunctionalTests.py: It wasn't really
    waiting for the process to finish running! So the results were invalid.
    This in turn exposes a bug in the test for align.tex, which now fails.
    Subsequent commit to fix the failure.

--- src/plasTeX/tests/__init__.py
+++ src/plasTeX/tests/__init__.py
@@ -1,4 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
 import subprocess
+import platform
+py_impl = getattr(platform, 'python_implementation', lambda: None)
+IS_PYPY = py_impl() == 'PyPy'
+
 from unittest import SkipTest
 
 def _real_check_for_binaries():
@@ -28,3 +38,68 @@ def skip_if_no_binaries():
     except OSError:
         _check_for_binaries = _already_checked_for_binaries_and_failed
         _already_checked_for_binaries_and_failed()
+
+
+if not IS_PYPY:
+    import os
+    import os.path
+    import sys
+
+    def run_plastex(tmpdir, filename, args=(), cwd=None):
+        skip_if_no_binaries()
+        # Run plastex on the document
+        # Must be careful to get the right python path so we work
+        # in tox virtualenvs as well as buildouts
+        path = os.path.pathsep.join(sys.path)
+        env = os.environ.copy()
+        env['PYTHONPATH'] = path
+        cmd = [
+            sys.executable,
+            '-m', 'plasTeX.plastex',
+            '-d', tmpdir
+        ]
+        cmd.extend(args)
+        cmd.append(filename)
+        __traceback_info__ = env, cmd
+        proc = subprocess.Popen( cmd,
+                                 env=env,
+                                 cwd=cwd,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE )
+        out, err = proc.communicate()
+        log = out + err
+        __traceback_info__ = env, cmd, log
+        if proc.returncode:
+            raise OSError("plastex failed with code %s:\n%s" % (proc.returncode, log))
+        return log
+else:
+    # spawning a new process is really slow under
+    # pypy, messing up all the jit work. It turns out to be much
+    # faster to fork a thread and wait for it...
+    # although, for some reason, it depends on the order of tests?
+    # Sometimes test_longtables takes 170s, sometimes it takes just a few
+
+    # This assumes we have good separation and don't pollute/alter global
+    # modules. This used to be a very false assumption but is getting more
+    # true as time goes on. Eventually this should become the default.
+
+    from plasTeX.plastex import main as _main
+    import threading
+    def run_plastex(tmpdir, filename, args=(), cwd=None):
+        skip_if_no_binaries()
+        cmd = ['plastex', '-d', tmpdir]
+        cmd.extend(args)
+        cmd.append(filename)
+        target = _main
+        if cwd:
+            def target(*args):
+                pwd = os.getcwd()
+                os.chdir(cwd)
+                try:
+                    _main(*args)
+                finally:
+                    os.chdir(pwd)
+        thread = threading.Thread(target=target,
+                                  args=(cmd,))
+        thread.start()
+        thread.join()
