1abdac34b2492baa6523a18a8673a68c30b01441
Carlos Sanchez <papachoco@gmail.com>
2017-08-26 13:29:30 -0500

    use six for chr func compat

--- src/plasTeX/_util.py
+++ src/plasTeX/_util.py
@@ -14,23 +14,39 @@ PY26 = sys.version_info[0:2] == (2, 6)
 PY27 = sys.version_info[0:2] == (2, 7)
 PYPY = hasattr(sys, 'pypy_translation_info')
 
-if PY2:
-    from __builtin__ import unichr as chr
-else:
-    import builtins
-    chr = builtins.chr
-
 from plasTeX.DOM import Node
 
+from six import unichr as chr
+chr_ = chr  # pylint
+
+if PY3:  # pragma: no cover
+    def _unicode(s): return str(s)
+else:
+    _unicode = unicode
+
+
+def text_(s, encoding='utf-8', err='strict'):
+    """
+    Decode a byte sequence and unicode result
+    """
+    s = s.decode(encoding, err) if isinstance(s, bytes) else s
+    return _unicode(s) if s is not None else None
+
+
 def subclasses(o):
-    """ Return all subclasses of the given class """
+    """ 
+    Return all subclasses of the given class
+    """
     output = [o]
     for item in o.__subclasses__():
         output.extend(subclasses(item))
     return output
 
+
 def sourceChildren(o, par=True):
-    """ Return the LaTeX source of the child nodes """
+    """ 
+    Return the LaTeX source of the child nodes
+    """
     if o.hasChildNodes():
         if par:
             return u''.join([x.source for x in o.childNodes])
@@ -41,17 +57,27 @@ def sourceChildren(o, par=True):
             return u''.join(source)
     return u''
 
+
 def sourceArguments(o):
-    """ Return the LaTeX source of the arguments """
+    """ 
+    Return the LaTeX source of the arguments 
+    """
     return o.argSource
 
+
 def ismacro(o):
-    """ Is the given object a macro? """
+    """ 
+    Is the given object a macro? 
+    """
     return hasattr(o, 'macroName')
 
+
 def issection(o):
-    """ Is the given object a section? """
+    """ 
+    Is the given object a section? 
+    """
     return o.level >= Node.DOCUMENT_LEVEL and o.level < Node.ENDSECTIONS_LEVEL
+
 
 def macroName(o):
     """ 
